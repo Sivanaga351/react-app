156577d41c708fad3a3522ae7f390748c0dc84c7
Sivanaga351 <sivanagak@birlasoft.com>
2022-03-14 07:53:42 +0530

    release

--- release.config.js
+++ release.config.js
@@ -2,9 +2,8 @@ module.exports={
     branches:"master",
     repositoryUrl: "https://github.com/Sivanaga351/react-app",
     Plugins:[
-        '@semantic-release/commit-analyzer', 
-        '@semantic-release/release-notes-generator', 
-        '@semantic-release/npm', 
-        '@semantic-release/github'
+        "@semantic-release/commit-analyzer", 
+        "@semantic-release/release-notes-generator", 
+        "@semantic-release/github"
     ]
 }
